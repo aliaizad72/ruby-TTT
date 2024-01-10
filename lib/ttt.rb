2ae347ca6b67591f9b415a20f8cf9597abb8e310
Ali Aizad <aliaizad72@gmail.com>
2024-01-10 09:25:13 +0800

    Initial transformation to OOP

--- lib/ttt.rb
+++ lib/ttt.rb
@@ -4,45 +4,68 @@
 class Player
   attr_reader :name, :symbol
 
-  def initialize(name, symbol)
-    @name = name
-    @symbol = symbol
+  def initialize
+    @name = ask_name
+    @symbol = ask_symbol
+  end
+
+  # Getting names from the players
+  def ask_name
+    print 'enter your name: '
+    gets.chomp
+  end
+
+  # Getting symbols from the players
+  def ask_symbol
+    print 'Enter your preferred symbol: '
+    gets.chomp
   end
 end
 
-# class IO to manage the flow of the game, I0 = input/output
-class IO
+# class Board to handle the operations and data needed to play the game
+class Board
+  attr_accessor :players, :state
+
+  def initialize
+    @players = add_players
+    @state = (1..9).to_a # A place to store the current ongoing game
+  end
+
+  def add_players
+    players = []
+    2.times do |i|
+      print "Player #{i + 1}, "
+      players.push(Player.new)
+    end
+    players
+  end
+
+  # Method to output the current game to the user
+  def display
+    state.each do |pos|
+      print " #{pos} "
+      print "\n" if (pos % 3).zero?
+    end
+  end
+
+  def play
+    display
+  end
+end
+
+# class Game to manage the flow of the game
+class Game
+  # The whole game flow is here
+  def play
+    intro
+    Board.new.play
+  end
+
   # Introduce the game and the rules
   def intro
     puts 'Welcome to Tic-Tac-Toe!'
   end
+end
 
-  # Getting names from the players
-  def ask_name(player_num)
-    print "Player #{player_num}, enter your name: "
-    gets.chomp
-  end
-end
-# Randomising symbol for each player
-symbols = %w[X O]
-player_1_symbol = symbols.delete(symbols.sample)
-player_2_symbol = symbols[0]
-# Randomising the turn of each player
-turn = [0, 1]
-player_1_turn = turn.delete(turn.sample)
-player_2_turn = turn[0]
-# Output the information to the players
-puts "#{player_1_name}, your symbol is #{player_1_symbol}."
-puts "#{player_2_name}, your symbol is #{player_2_symbol}."
-# A place to store the current ongoing game
-current_game = (1..9).to_a
-# Method to output the current game to the user
-def display(array)
-  array.each do |pos|
-    print " #{pos} "
-    print "\n" if (pos % 3).zero?
-  end
-end
-display current_game
-# Play the game
-# Check which player's turn is it
+Game.new.play
+
